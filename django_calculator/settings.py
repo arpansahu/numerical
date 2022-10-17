2b24a4b23f22bb600d44ecd9fc3c26bc93a9a99c
arpan sahu <arpanrocks95@gmail.com>
2022-10-17 22:57:48 +0530

    updated requirements.txt

--- django_calculator/settings.py
+++ django_calculator/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'django_calculator.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
